9f445d85d31d0b82dc0e744629dc93dac64effdf
Ray Computer <rlw950@yahoo.com>
2021-09-26 14:19:41 -0700

    added 6 images and array with book info

---
+++ array.js
@@ -0,0 +1,50 @@
+info: [
+{
+    title: 'Harlem Shuffle',
+    author: 'Colson Whitehead',
+    date: 'September 14, 2021',
+    price: '$22.95',
+    reviewerName: 'John Doe',
+    review: 'I found the book to be a great mystery and it was enjoyable to read.'
+},
+{
+    title: 'Bewilderment',
+    author: 'Richard Powers',
+    date: 'September 21, 2021',
+    price: '$22.49',
+    reviewerName: 'John Doe',
+    review: 'An incredible, unforgettable story of childhood, the natural world, and an unbreakable bond between parent and child that's perfect for right now.'
+},
+{
+    title: 'Cloud Cuckoo Land',
+    author: 'Anthony Doerr',
+    date: 'September 28, 2021',
+    price: '$23.99',
+    reviewerName: 'John Doe',
+    review: "Pacific Rim meets The Handmaid's Tale in this blend of Chinese history and mecha science fiction for YA readers"
+},
+{
+    title: 'Iron Widow',
+    author: 'Xiran Jay Zhao',
+    date: 'September 21, 2021',
+    price: '$14.98',
+    reviewerName: 'John Doe',
+    review: 'I found the book to be a great mystery and it was enjoyable to read.'
+},
+{
+    title: 'Hell of a Book',
+    author: 'Jason Mott',
+    date: 'June 29, 2021',
+    price: '$14.99',
+    reviewerName: 'John Doe',
+    review: 'An astounding work of fiction from a New York Times bestselling author Jason Mott, always deeply honest, at times electrically funny, that goes to the heart of racism, police violence, and the hidden costs exacted upon Black Americans, and America as a whole'
+},
+{
+    title: 'The Sovenir Museum',
+    author: 'Elizabeth McCracken',
+    date: 'April 13, 2021',
+    price: '$23.99',
+    reviewerName: 'John Doe',
+    review: 'In these stories, the mysterious bonds of family are tested, transformed, fractured, and fortified.'
+},
+]
